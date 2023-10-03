c44c26026789c3c82e2ed1e1f60f92859cf3680d
mahfuj-jim <mahfuj.ahmed@bjitacademy.com>
2023-10-03 10:19:16 +0600

    Signup Page from Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,35 @@
+import { Link } from "react-router-dom";
 import "./header.style.css";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <nav className="navbar">
-                <a>Online Book Store</a>
-                <div className="nav-btn">
-                    <ul>
-                        <li><a href>Home</a></li>
-                        <li><a href="#services">Books</a></li>
-                        <li><a href="#office">New Release</a></li>
-                        <li><a href="#academy">Blog</a></li>
-                        <li><a href="#footer">Contacts</a></li>
-                    </ul>
-                </div>
-            </nav>
+  return (
+    <div className="header">
+      <nav className="navbar">
+        <a>Online Book Store</a>
+        <div className="nav-btn">
+          <ul>
+            <li>
+              <a href>Home</a>
+            </li>
+            <li>
+              <a href="#services">Books</a>
+            </li>
+            <li>
+              <a href="#office">New Release</a>
+            </li>
+            <li>
+              <a href="#academy">Blog</a>
+            </li>
+            <Link to={`/signup`}>
+              <li>
+                <a href="#footer">Signup</a>
+              </li>
+            </Link>
+          </ul>
         </div>
-    );
-}
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
